1fa669ffa79a9542386983f9faddef2290d61305
Rishi Bedi <rbedi@cs.stanford.edu>
2016-12-09 05:34:39 -0800

    npz generated for A, B

--- extract_coords.py
+++ extract_coords.py
@@ -1,7 +1,7 @@
 import vmd
 import atomsel
 import numpy as np
-
+import sys
 
 def align_frames(reference, target):
     '''
@@ -31,18 +31,20 @@ def export_sel(molid, sel_descriptor):
     the selection of atoms specified by sel
     '''
     sel_pos = []
-    num_frames = vmd.molecule.numframes(molid)
+    num_frames = 4  #vmd.molecule.numframes(molid)
     for frame in range(num_frames):
-        frame_data = vmd.vmdnumpy.timestep(molid, frame)
+        frame_data = np.copy(vmd.vmdnumpy.timestep(molid, frame))
         sel = vmd.vmdnumpy.atomselect(molid, frame, sel_descriptor)
-        sel_pos.append(np.take(frame_data, np.nonzero(sel), 0)[0][0])
+        sel_pos.append(np.take(frame_data, np.nonzero(sel), 0)[0])
 
-    return np.array(sel_pos)
+    return np.array(sel_pos).squeeze()
 
+sim_id = sys.argv[1]
+sim_path = sys.argv[2]
 
 # Load the trajectory
 ROOT = '/scratch/PI/rondror/DesRes-Simulations/Downloaded/M2R_Nature_Dror_Desres'
-TRAJ = 'DESRES-Trajectory_nature2013-A/DESRES-Trajectory_nature2013-A-01-sel1/nature2013-A-01-sel1'
+TRAJ = 'DESRES-Trajectory_nature2013-' + sim_id + '/DESRES-Trajectory_' + sim_path + '/' + sim_path
 PATH = ROOT+'/'+TRAJ+'/'
 traj_name = TRAJ.split('/')[-1]
 
@@ -51,20 +53,23 @@ vmd.molecule.load('mae', PATH+traj_name+'.mae', 'dcd', PATH+traj_name+'-000.dcd'
 # Align the frames to the first frame of the simulation
 align_frames(0, 0)
 
-# Extract three coordinates of the ligand
-# These values are specific to trajectory A
-center_ligand_pos = export_sel(0, 'index 2230')  # corresponds to a C2 on C73P
-side1_ligand_pos = export_sel(0, 'index 2232')  # corresponds to a C on one side of C73P
-side2_ligand_pos = export_sel(0, 'index 2212')  # corresponds to a C on the other side of C73P
 
-# Extract the centroid of the binding site
-# These values are also specific to trajectory A
+if sim_id == 'A' or sim_id == 'B':
+    # Extract three coordinates of the ligand
+    # These values are specific to trajectory A
+    ligand_pos = export_sel(0, 'resname C73P')
+    center_ligand_pos = ligand_pos[:, 18, :]    # corresponds to C2 on C73P (index 2230 in A.mae)
+    side1_ligand_pos = ligand_pos[:, 20, :]    # corresponds to C on C73P (index 2232 in A.mae)
+    side2_ligand_pos = ligand_pos[:, 0, :]     # corresponds to C on C73P (index 2212 in A.mae)
 
-receptor_1_pos = export_sel(0, 'index 1837')  # CA on ASN residue 410
-receptor_2_pos = export_sel(0, 'index 1241')  # CA on TYR residue 177
-receptor_3_pos = export_sel(0, 'index 1924')  # CA on TRP residue 422
+    # Extract the centroid of the binding site
+    # These values are also specific to trajectory A
+    receptor_1_pos = export_sel(0, 'resid 410 and name CA')  # CA on ASN residue 410
+    receptor_2_pos = export_sel(0, 'resid 127 and name CA')  # CA on TYR residue 177
+    receptor_3_pos = export_sel(0, 'resid 422 and name CA')  # CA on TRP residue 422
 
-np.savez('trajA_coords',
+
+np.savez(sim_path + '_coords',
          center_ligand_pos=center_ligand_pos,
          side1_ligand_pos=side1_ligand_pos,
          side2_ligand_pos=side2_ligand_pos,
